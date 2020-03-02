d24212fb55dfecdbb494daca99e258a97da540f1
SteRobWms <stephen.r.williams.87@gmail.com>
2020-03-02 16:28:03 -0600

    relationships galore

--- blackjack/app/models/dealer_hand.rb
+++ blackjack/app/models/dealer_hand.rb
@@ -1,4 +1,6 @@
 class DealerHand < ApplicationRecord
   belongs_to :dealer
   belongs_to :game
+  has_many :dealer_hand_cards
+  has_many :cards, through: :dealer_hand_cards
 end
--- blackjack/app/models/game.rb
+++ blackjack/app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+    has_one :deck
+    has_many :player_hands
+    has_many :players, through: :player_hands
+    has_one :dealer_hand
+    has_one :dealer, though: :dealer_hand
 end

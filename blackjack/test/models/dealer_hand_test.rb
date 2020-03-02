327354f090d374b432e5ae5e523be6a1f25e0cf6
vienpham2019 <vienpham2019@gmail.com>
2020-03-02 16:29:40 -0600

    save

---
+++ blackjack/test/models/dealer_hand_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DealerHandTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ blackjack/test/models/player_hand_card_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PlayerHandCardTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

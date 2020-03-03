14431b25567eb2d272dc7ac602db2939f42bc351
SteRobWms <stephen.r.williams.87@gmail.com>
2020-03-03 09:36:04 -0600

    Tuesday AM commit

---
+++ blackjack/app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+class GamesController < ApplicationController
+  before_action :current_game, only: [:show]
+  
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+  end
+
+  def show
+  end
+
+  def card_to_numbers(card)
+    if card[:value].to_i
+      card_value = card[:value].to_i
+    elsif card[:value] == "A"
+      card_value = 11
+    else card[:value] = 10
+    end
+  end
+
+  private
+  def current_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:min_bet, :max_bet, :players)
+  end
+
+  def who_won(hand_1, hand_2)
+    
+  end
+  
+
+end

5b1ea6cd0f616fa1dcdf73d217ddd3c44b2e201e
SteRobWms <stephen.r.williams.87@gmail.com>
2020-03-03 18:22:36 -0600

    day 2 evening commit

--- blackjack/app/models/game.rb
+++ blackjack/app/models/game.rb
@@ -1,8 +1,10 @@
 class Game < ApplicationRecord
     has_many :player_hands
     has_many :players, through: :player_hands
+    # accepts_nested_attributes_for :player_hands
     has_many :player_hand_cards, through: :player_hands
     has_one :dealer_hand
+    # accepts_nested_attributes_for :dealer_hand
     has_one :dealer, through: :dealer_hand
     has_many :dealer_hand_cards, through: :dealer_hand
 

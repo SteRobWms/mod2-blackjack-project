0634c291b9eb0bc99d70efcb045461e4d981d3d6
SteRobWms <stephen.r.williams.87@gmail.com>
2020-03-06 13:29:14 -0600

    working version, minus some styling

--- blackjack/app/models/player.rb
+++ blackjack/app/models/player.rb
@@ -7,4 +7,21 @@ class Player < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :age, presence: true, numericality: { only_integer: true , greater_than: 18}
     validates :security_answer, presence: true 
+
+    def total_hand_count
+        self.player_hands.count
+    end
+
+    def outcomes
+        self.player_hands.map{|hand| hand.outcome}
+    end
+
+    # def outcome_counts
+    #     outcome_hash = {"Blackjack" => 0, "Win" => 0, "Push" => 0, "Loss" => 0, "Dealer Blackjack" => 0}
+    #     self.player_hands.each do |hand|
+    #         outcome_hash[hand.outcome] += 1
+    #     end
+    #     outcome_hash
+    # end
+
 end
